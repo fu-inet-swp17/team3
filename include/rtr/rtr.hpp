113751262c6d3d6494d0df41c0d0260e5ab7fe27
Sebastian Sontberg <sebastian@sontberg.de>
2017-05-20 04:30:13 +0200

    rtr: add simple validate function

--- include/rtr/rtr.hpp
+++ include/rtr/rtr.hpp
@@ -7,71 +7,71 @@ extern "C" {
 #include "rtrlib/rtrlib.h"
 }
 
-static void status_fp(const struct rtr_mgr_group *group __attribute__((unused)),
-                      enum rtr_mgr_status mgr_status,
-                      const struct rtr_socket *rtr_sock,
-                      void *data __attribute__((unused)))
-{
-    BOOST_LOG_TRIVIAL(info)
-        << "RTR-Socket changed connection status to: "
-        << rtr_state_to_str(rtr_sock->state)
-        << "Mgr Status: "
-        << rtr_mgr_status_to_str(mgr_status);
-}
+// static void status_fp(const struct rtr_mgr_group *group __attribute__((unused)),
+//                       enum rtr_mgr_status mgr_status,
+//                       const struct rtr_socket *rtr_sock,
+//                       void *data __attribute__((unused)))
+// {
+//     BOOST_LOG_TRIVIAL(info)
+//         << "RTR-Socket changed connection status to: "
+//         << rtr_state_to_str(rtr_sock->state)
+//         << "Mgr Status: "
+//         << rtr_mgr_status_to_str(mgr_status);
+// }
 
-static void update_cb(struct pfx_table *p  __attribute__((unused)),
-		      const struct pfx_record rec,
-		      const bool added)
-{
-    char ip[INET6_ADDRSTRLEN];
+// static void update_cb(struct pfx_table *p  __attribute__((unused)),
+// 		      const struct pfx_record rec,
+// 		      const bool added)
+// {
+//     char ip[INET6_ADDRSTRLEN];
 
-    if (added)
-        printf("+ ");
-    else
-        printf("- ");
-    lrtr_ip_addr_to_str(&rec.prefix, ip, sizeof(ip));
-    printf("%-40s   %3u - %3u   %10u\n",
-           ip, rec.min_len, rec.max_len, rec.asn);
-}
+//     if (added)
+//         printf("+ ");
+//     else
+//         printf("- ");
+//     lrtr_ip_addr_to_str(&rec.prefix, ip, sizeof(ip));
+//     printf("%-40s   %3u - %3u   %10u\n",
+//            ip, rec.min_len, rec.max_len, rec.asn);
+// }
 
-static void update_spki(struct spki_table *s __attribute__((unused)),
-			const struct spki_record record,
-			const bool added)
-{
-    char c;
+// static void update_spki(struct spki_table *s __attribute__((unused)),
+// 			const struct spki_record record,
+// 			const bool added)
+// {
+//     char c;
 
-    if (added)
-        c = '+';
-    else
-        c = '-';
+//     if (added)
+//         c = '+';
+//     else
+//         c = '-';
 
-    printf("%c ", c);
-    printf("ASN:  %u\n  ", record.asn);
+//     printf("%c ", c);
+//     printf("ASN:  %u\n  ", record.asn);
 
-    int i;
-    int size = sizeof(record.ski);
+//     int i;
+//     int size = sizeof(record.ski);
 
-    printf("SKI:  ");
-    for (i = 0; i < size; i++) {
-        printf("%02x", record.ski[i]);
-        if (i < size - 1)
-            printf(":");
-    }
-    printf("\n  ");
+//     printf("SKI:  ");
+//     for (i = 0; i < size; i++) {
+//         printf("%02x", record.ski[i]);
+//         if (i < size - 1)
+//             printf(":");
+//     }
+//     printf("\n  ");
 
-    i = 0;
-    size = sizeof(record.spki);
-    printf("SPKI: ");
-    for (i = 0; i < size; i++) {
-        if ((i % 40 == 0) && (i != 0))
-            printf("\n	");
+//     i = 0;
+//     size = sizeof(record.spki);
+//     printf("SPKI: ");
+//     for (i = 0; i < size; i++) {
+//         if ((i % 40 == 0) && (i != 0))
+//             printf("\n	");
 
-        printf("%02x", record.spki[i]);
-        if (i < size - 1)
-            printf(":");
-    }
-    printf("\n");
-}
+//         printf("%02x", record.spki[i]);
+//         if (i < size - 1)
+//             printf(":");
+//     }
+//     printf("\n");
+// }
 
 namespace RTR {
 
@@ -98,7 +98,7 @@ namespace RTR {
             groups[0].preference = 1;
 
             int ret = rtr_mgr_init(&conf, groups, 1, 30, 600, 600,
-                                   update_cb, update_spki, status_fp, NULL);
+                                   NULL, NULL, NULL, NULL);
 
             if (ret == RTR_ERROR)
                 BOOST_LOG_TRIVIAL(warning) << "Error in rtr_mgr_init";
@@ -115,6 +115,28 @@ namespace RTR {
 
         void stop(void) {
             rtr_mgr_stop(conf);
+        }
+
+        int validate(const uint32_t asn, const struct lrtr_ip_addr* prefix, const uint8_t mask_len) {
+
+            enum pfxv_state result;
+            
+            int ret = rtr_mgr_validate(conf, asn, prefix, mask_len, &result);
+
+            if (ret == PFX_SUCCESS) {
+                if (result == BGP_PFXV_STATE_VALID) {
+                    BOOST_LOG_TRIVIAL(info) << "MTR: valid";
+                } else if (result == BGP_PFXV_STATE_INVALID) {
+                    BOOST_LOG_TRIVIAL(info) << "MTR: invalid";
+                } else if (result == BGP_PFXV_STATE_NOT_FOUND) {
+                    BOOST_LOG_TRIVIAL(info) << "MTR: no certificate found";
+                }
+            } else {
+                BOOST_LOG_TRIVIAL(info) << "MTR: call failed";
+                return -1;
+            }
+
+            return result;
         }
 
         ~Manager(void) {
