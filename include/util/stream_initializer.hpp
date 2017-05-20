8a977499a1f21d09dfcdb1b66582623f5ffbb31d
Sebastian Sontberg <sebastian@sontberg.de>
2017-05-20 03:16:36 +0200

    util: expand stream_from_options initializer
    Now supports specifying the data interface and interface options and
    thus allows reliable testing with the 'csvfile' metadata interface.

--- include/util/stream_initializer.hpp
+++ include/util/stream_initializer.hpp
@@ -28,25 +28,49 @@ namespace util {
     BGP::Stream stream_from_options(ProgramOptions& opts) {
         BGP::Stream stream;
 
-        // FIXME: not implemented yet
-        if (auto interface = opts.interface()) {
-            std::cout << "Interface: " << *interface << std::endl;
+        // Set interface if given
+        if (auto iface = opts.interface())
+            stream.set_interface(*iface);
+ 
+        // Print out description of the interface set
+        BOOST_LOG_TRIVIAL(info) << "data interface: "
+                                << stream.get_interface().first
+                                << " (" << stream.get_interface().second << ")"
+                                << std::endl;
 
-            for (auto& opt_value : opts.interface_options())
-                std::cout << "Interface option '"
-                          << opt_value.first << "' = '"
-                          << opt_value.second << "'" << std::endl;
+        // Set interface options if any given
+        for (auto& opt : opts.interface_options()) {
+            try {
+                stream.set_interface_option(opt.first, opt.second);
+
+                BOOST_LOG_TRIVIAL(info) << "  option: " << opt.first
+                                        << " = " << opt.second
+                                        << std::endl;
+                
+            } catch (BGP::config_error& e) {
+                BOOST_LOG_TRIVIAL(warning) << "unknown option '" << opt.first << "'";
+            }
         }
-
-        // FIXME: not implemented yet
-        for (auto& interval : opts.intervals())
-            std::cout << "Start: " << interval.first << ", End: " << interval.second << std::endl;
+        
+        // Setup time window intervals
+        for (auto& interval : opts.intervals()) {
+            // 0 as end of interval means live mode
+            if (interval.second == 0)
+                stream.add_interval(interval.first);
+            else
+                stream.add_interval(interval.first, interval.second);
+        }
         
         // Add project filters
         if (auto project_filters = opts.project_filters())
             for (auto &p : *project_filters)
                 stream.add_filter(BGP::Filter::Project, p);
 
+        // Add collector filters
+        if (auto collector_filters = opts.collector_filters())
+            for (auto &c : *collector_filters)
+                stream.add_filter(BGP::Filter::Collector, c);
+        
         return stream;        
     }
 }
