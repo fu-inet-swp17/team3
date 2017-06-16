473d365e48d8e898aa50353156066edcbb75a8a1
Sebastian Sontberg <sebastian@sontberg.de>
2017-06-16 04:27:30 +0200

    fuzzbgp: use StreamController and clean up

--- src/fuzzbgp.cpp
+++ src/fuzzbgp.cpp
@@ -1,109 +1,117 @@
 #include <iostream>
-#include <chrono>
-#include <ctime>
-#include <thread>
 #include <iomanip>
-#include <string>
-#include <algorithm>
-#include <map>
-#include <tuple>
-#include <sstream>
-#include <unordered_set>
 
-#include <boost/log/core.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/expressions.hpp>
 
-#include "bgp/bgp.hpp"
-#include "rtr/rtr.hpp"
+#include "stream_controller.hpp"
+#include "prefix_store.hpp"
 
-#include "util/formatters.hpp"
 #include "util/program_options.hpp"
 #include "util/stream_initializer.hpp"
 
-#include "collector_stream.hpp"
-#include "prefix_store.hpp"
-
 #include "config.hpp"
 
-using util::ProgramOptions;
+using namespace util;
 
-struct PrefixStatus {
-    uint32_t vp;
-    uint32_t origin_as;
-};
-
-typedef std::map<bgpstream_pfx_storage_t, std::list<PrefixStatus>> pfx_tree_t;
-
+void process_element(PrefixStore& pfx_store, unsigned collector, const BGP::Element& e);
 
 int main(int argc, char** argv) {
+    // Set log level
     boost::log::core::get()->set_filter(boost::log::trivial::severity >= boost::log::trivial::info);
 
+    // Parse command line (largely compatible to bgpreader)
     ProgramOptions opts(argc, argv);
 
+    // Exit if program options say so
     if (auto ret = opts.exit()) return *ret;
 
-    CollectorStream stream("route-views2");
-    stream.start(std::time(nullptr) - (60 * 60 * 8));
+    // Construct StreamController from BGP::Stream defined by program options
+    StreamController sc(stream_from_options(opts));
 
-    // Use this for csv-file input
-    // BGP::Stream stream = util::stream_from_options(opts);
-    // stream.start();
+    // Start stream controller
+    sc.start();
 
+    // Stores the prefixes
     PrefixStore pfx_store;
 
-    unsigned records = 0;
+    // Stats
+    unsigned processed = 0, ignored = 0;
 
+    // Reused per loop
     BGP::Record r;
 
-    while (stream.next(r)) {
+    // Ignore / Flush / Process for each Record
+    StreamController::Instruction op;
 
-        if (r.status() == BGP::Record::Status::Valid) {
+    // Numerical identifier for the collector
+    unsigned collector;
 
-            for (const auto& e : r) {
-                 switch (e.type()) {
-                 case BGP::Element::Type::RIB: {
-                      pfx_store.add_rib_element(e);
-                      break;
-                 }
-                 case BGP::Element::Type::Announcement: {
-                      pfx_store.add_announcement(e);
-                      break;
-                 }
-                 case BGP::Element::Type::Withdrawal: {
-                      pfx_store.add_withdrawal(e);
-                      break;
-                 }
-                 case BGP::Element::Type::PeerState: {
-                      const std::time_t& tm = e.timestamp();
+    while (true) {
 
-                      // FIXME: not handled yet
-                      BOOST_LOG_TRIVIAL(debug) << std::put_time(std::gmtime(&tm), "%F %T") << ": "
-                                               << "Peer state of " << e.peer_asnumber() << " changed from "
-                                               << "" << e.peer_state().first << " to " << e.peer_state().second;
-                      break;
-                 }
-                 default: {
-                      const std::time_t& tm = e.timestamp();
+        // Get instruction & collector number
+        std::tie(op, collector) = sc.next(r);
 
-                      BOOST_LOG_TRIVIAL(warning) << std::put_time(std::gmtime(&tm), "%F %T") << ": "
-                                                 << "Invalid element type!";
-                      break;
-                 }
-                 }
-            }
+        switch (op) {
+            case StreamController::Instruction::Ignore:
+                ignored++;
+                break;
+
+            case StreamController::Instruction::Flush:
+                pfx_store.flush(collector);
+
+            case StreamController::Instruction::Process:
+                processed++;
+
+                // Loop through individual BGP::Elements and process
+                for (const auto& e : r)
+                    process_element(pfx_store, collector, e);
         }
 
-        else {
-             const std::time_t& tm = r.record_time();
-
-             BOOST_LOG_TRIVIAL(warning) << std::put_time(std::gmtime(&tm), "%F %T") << ": "
-                                        << "Record not valid!";
+        // Print stats every 10 000 records
+        if (!((processed + ignored) % 10000)) {
+            BOOST_LOG_TRIVIAL(info) << processed << " records processed (" << ignored << " ignored)";
         }
-
-        if (!(++records % 10000))
-            BOOST_LOG_TRIVIAL(info) << records << " records processed.";
     }
 
     return 0;
+}
+
+void process_element(PrefixStore& pfx_store, unsigned collector, const BGP::Element& e) {
+
+    switch (e.type()) {
+
+        case BGP::Element::Type::RIB: {
+            pfx_store.add_rib_element(collector, e);
+            break;
+        }
+
+        case BGP::Element::Type::Announcement: {
+            pfx_store.add_announcement(collector, e);
+            break;
+        }
+
+        case BGP::Element::Type::Withdrawal: {
+            pfx_store.add_withdrawal(collector, e);
+            break;
+        }
+
+        case BGP::Element::Type::PeerState: {
+            const std::time_t& tm = e.timestamp();
+
+            // FIXME: not handled yet
+            BOOST_LOG_TRIVIAL(debug) << std::put_time(std::gmtime(&tm), "%F %T") << ": "
+                                     << "Peer state of " << e.peer_asnumber() << " changed from "
+                                     << "" << e.peer_state().first << " to " << e.peer_state().second;
+            break;
+        }
+
+        default: {
+            const std::time_t& tm = e.timestamp();
+
+            BOOST_LOG_TRIVIAL(warning) << std::put_time(std::gmtime(&tm), "%F %T") << ": "
+                                       << "Invalid element type!";
+            break;
+        }
+    }
 }
