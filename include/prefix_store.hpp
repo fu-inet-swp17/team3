3aa0dbbbd469cdadd33fd950fe11ed83e045f81b
Sebastian Sontberg <sebastian@sontberg.de>
2017-06-21 07:34:03 +0200

    fuzzbgp: add basic outage output
    needs improvement

--- include/prefix_store.hpp
+++ include/prefix_store.hpp
@@ -3,6 +3,7 @@
 
 #include <list>
 #include <map>
+#include <set>
 
 #include "bgp/bgp.hpp"
 #include "split_map.hpp"
@@ -15,16 +16,23 @@ class PrefixStore {
         prefix_status(uint32_t v, uint32_t o, uint32_t c) : vp(v), origin(o), collector(c) {}
     };
 
+    typedef std::function<void(bgpstream_pfx_storage_t, std::uint32_t, std::time_t, const std::list<bgpstream_pfx_storage_t>&)> lost_prefix_cb_t;
+    
 private:
 
     prefix::split_map<std::list<prefix_status>> pfx_map;
 
+    std::map<std::uint32_t, std::list<bgpstream_pfx_storage_t>> active_prefixes;
+    std::map<std::uint32_t, std::list<bgpstream_pfx_storage_t>> lost_prefixes;
+
     void upsert_status(bgpstream_pfx_storage_t pfx, std::uint32_t vp, std::uint32_t origin, std::uint32_t collector, std::time_t tm);
     void delete_status(bgpstream_pfx_storage_t pfx, std::uint32_t vp, std::uint32_t origin, std::uint32_t collector, std::time_t tm);
 
+    lost_prefix_cb_t lost_prefix_cb;
+    
 public:
 
-    PrefixStore(void);
+    PrefixStore(lost_prefix_cb_t&& cb);
 
     void flush(unsigned collector);
 
--- src/prefix_store.cpp
+++ src/prefix_store.cpp
@@ -5,13 +5,13 @@
 
 using util::format_prefix;
 
-PrefixStore::PrefixStore(void) {
+PrefixStore::PrefixStore(lost_prefix_cb_t&& cb) : lost_prefix_cb(cb) {
     BOOST_LOG_TRIVIAL(trace) << "PrefixStore::PrefixStore()";
 }
 
 void PrefixStore::flush(unsigned collector) {
     BOOST_LOG_TRIVIAL(info) << "Flushing collector #" << collector;
-    
+
     for (auto it = pfx_map.begin4(); it != pfx_map.end4(); it++) {
 
         std::remove_if((*it).second.begin(), (*it).second.end(),
@@ -23,7 +23,7 @@ void PrefixStore::flush(unsigned collector) {
     BOOST_LOG_TRIVIAL(info) << "Done";
 }
 
-void PrefixStore::upsert_status(bgpstream_pfx_storage_t pfx, std::uint32_t vp, std::uint32_t origin, std::uint32_t collector, std::time_t) {
+void PrefixStore::upsert_status(bgpstream_pfx_storage_t pfx, std::uint32_t vp, std::uint32_t origin, std::uint32_t collector, std::time_t tm) {
 
     auto& l = pfx_map[pfx];
 
@@ -35,10 +35,28 @@ void PrefixStore::upsert_status(bgpstream_pfx_storage_t pfx, std::uint32_t vp, s
     if (m == l.end()) {
 
         l.emplace_back(vp, origin, collector);
-        
+
     } else if (m->origin != origin) {
 
         m->origin = origin;
+    }
+
+    auto& active = active_prefixes[origin];
+    auto& lost = lost_prefixes[origin];
+
+    if (std::find(active.begin(), active.end(), pfx) == active.end()) {
+        active.push_back(pfx);
+
+        auto rem = std::remove(lost.begin(), lost.end(), pfx);
+
+        if (rem != lost.end()) {
+            lost.erase(rem, lost.end());
+
+            BOOST_LOG_TRIVIAL(info) << std::put_time(std::gmtime(&tm), "%F %T") << ": "
+                                    << "AS" << std::setw(10) << std::left << origin << " RECOV "
+                                    << format_prefix(pfx) << " "
+                                    << "(" << active.size() << " left, " << lost.size() << " lost)";
+        }
     }
 }
 
@@ -64,7 +82,7 @@ void PrefixStore::delete_status(bgpstream_pfx_storage_t pfx, std::uint32_t vp, s
         } else {
 
             auto origin = (*m).origin;
-            
+
             l.erase(m);
 
             BOOST_LOG_TRIVIAL(debug) << "* " << format_prefix(pfx);
@@ -77,9 +95,36 @@ void PrefixStore::delete_status(bgpstream_pfx_storage_t pfx, std::uint32_t vp, s
             }
 
             if (l.empty()) {
-                BOOST_LOG_TRIVIAL(info) << std::put_time(std::gmtime(&tm), "%F %T") << ": "
-                                        << "Lost AS" << std::setw(12) << std::left
-                                        << origin << " " << format_prefix(pfx);
+
+                auto& active = active_prefixes[origin];
+                auto& lost = lost_prefixes[origin];
+
+                auto del = std::remove(active.begin(), active.end(), pfx);
+
+                if (del != active.end()) {
+                    lost.push_back(pfx);
+                    active.erase(del, active.end());
+                }
+
+                auto outer = std::find_if(active.begin(), active.end(),
+                                          [&] (const bgpstream_pfx_storage_t other) {
+
+                                              return (bgpstream_pfx_contains((bgpstream_pfx_t *)&other,
+                                                                             (bgpstream_pfx_t *)&pfx) != 0);
+                                          });
+
+                // No containing prefix found
+                if (outer == active.end()) {
+
+                    BOOST_LOG_TRIVIAL(info) << std::put_time(std::gmtime(&tm), "%F %T") << ": "
+                                            << "AS" << std::setw(10) << std::left << origin << " LOST  "
+                                            << format_prefix(pfx) << " "
+                                            << "(" << active.size() << " left, " << lost.size() << " lost)";
+
+                    if (active.size() == 0) {
+                        lost_prefix_cb(pfx, origin, tm, lost);
+                    }
+                }
             }
         }
     }
